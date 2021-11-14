001e9d8ddbbe32b748d12601d54eea7ab068b828
thetottyapple <74386430+thetottyapple@users.noreply.github.com>
2021-11-14 09:53:44 -0600

    config.ts
    metadata fix

--- src/config.ts
+++ src/config.ts
@@ -12,13 +12,21 @@ type MetaType = {
 };
 
 const META: MetaType = {
-	title: "NextJS ChakraUI Template | Hazim Arafa",
+	title: "HibatIQ",
 	lang: "en-us",
-	description:
-		"Boilerplate built to scale containing Typescript + NextJS + ChakraUI + Google Analytics + ESLint + Jest + Styled Components + Icons",
-	url: "https://boilerplate.hazimarafa.tech",
+	description: "HibatIQ L.T.D.",
+	url: "https://hibatiq.com",
 	image: "/logo.png",
-	tags: ["nextjs", "typescript", "chakraui", "google analytics", "eslint", "jest", "styled components", "icons"]
+	tags: [
+		"nextjs",
+		"typescript",
+		"chakraui",
+		"google analytics",
+		"eslint",
+		"jest",
+		"styled components",
+		"icons",
+	],
 };
 
 export { GA_TRACKING_ID, IS_PRODUCTION, META };
